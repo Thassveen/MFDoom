7a91923127f091120c201e798c363a3fe163b223
Thassveen <veenboys1928@gmail.com>
2024-06-04 16:25:18 +0800

    health

--- Desktop/doomy/main.py
+++ Desktop/doomy/main.py
@@ -14,11 +14,6 @@ pygame.display.set_caption("MFDoom")
 clock = pygame.time.Clock() 
 FPS = 60 
  
-#define colours
-RED = (255, 0, 0)
-YELLOW = (255, 255, 0)
-WHITE = (255, 255, 255) 
-
 #load bg image 
 bg_image = pygame.image.load("assets/images/background/background.jpg").convert_alpha() 
  
@@ -26,18 +21,11 @@ bg_image = pygame.image.load("assets/images/background/background.jpg").convert_
 def draw_bg(): 
     scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH,SCREEN_HEIGHT)) 
     screen.blit(scaled_bg, (0,0)) 
-
-#function for drawing fighter health bars
-def draw_health_bar(health, x, y):
-    ratio = health / 100
-    pygame.draw.rect(screen, WHITE, (x - 1, y - 1, 404, 32))
-    pygame.draw.rect(screen, RED, (x, y, 400, 30))
-    pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
-
-
-#create two instances of fighters 
+ 
+ 
+#create two  of fighters 
 fighter_1 = Fighter(200,310)     
-fighter_2 = Fighter(700,310)  
+#fighter_2 = Fighter(700,310)  
  
 #game loop 
 run = True 
@@ -47,18 +35,14 @@ while run:
  
     #draw bg 
     draw_bg() 
-
-    #show player stats
-    draw_health_bar(fighter_1.health, 20, 20)
-    draw_health_bar(fighter_2.health, 580, 20)
-
+ 
     #move fighters 
-    fighter_1.move(SCREEN_WIDTH,SCREEN_HEIGHT, screen, fighter_2) 
-    fighter_2.move(SCREEN_WIDTH,SCREEN_HEIGHT, screen, fighter_1)
-
+    fighter_1.move(SCREEN_WIDTH,SCREEN_HEIGHT) 
+    #fighter_2.move() 
+ 
     #draw fighters 
     fighter_1.draw(screen) 
-    fighter_2.draw(screen) 
+    #fighter_2.draw(screen) 
  
     #event handler 
     for event in pygame.event.get(): 
